1e81b11d26580539c1d5ffee9c5b4f308ddba559
Sergey Mitrofanov <mitrofanovsergey2001@gmail.com>
2024-04-12 03:17:31 +0300

    First commit

---
+++ rostelecom_bot/main.py
@@ -0,0 +1,31 @@
+"""
+Файл с реализацией рабочего интерфейса телеграм-бота
+"""
+import telebot
+from telebot import types
+
+
+token = '7088641468:AAEpy3fA7WSCfuZf00vp4NiKSjnELh5LmQE'  # Замените 'YOUR_TOKEN' на ваш токен бота
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton('Администратор', callback_data='button1')
+    button2 = types.InlineKeyboardButton('Пользователь', callback_data='button2')
+    keyboard.add(button1, button2)
+
+    bot.send_message(message.chat.id, 'Выберите профиль:', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data == 'button1':
+        bot.send_message(call.message.chat.id, 'Введите пароль')
+    elif call.data == 'button2':
+        bot.send_message(call.message.chat.id, 'Что я могу для вас сделать?')
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
