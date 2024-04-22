2e25772d2e704452eccd5fe8c25bbe14888969b5
Sergey Mitrofanov <mitrofanovsergey2001@gmail.com>
2024-04-23 02:40:52 +0300

    improved the ability to check Y.disk and delete files from Y.disk; fixed some problems in async connection to Y.disk; bot phrases updated

--- rostelecom_bot/handlers/common_handlers.py
+++ rostelecom_bot/handlers/common_handlers.py
@@ -15,7 +15,7 @@ from aiogram.utils.formatting import Text, Bold
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
-from rostelecom_bot.utils.config import file_extension
+from rostelecom_bot.utils.config import file_extension, configuration
 
 
 class AdminsList:
@@ -56,7 +56,7 @@ async def cmd_start(message: types.Message, state: FSMContext):
 @router.message(AuthStates.ADMIN, Command("user"))
 async def switch_to_user(message: types.Message, state: FSMContext):
     if message.from_user.id not in AdminsList.ADMIN_ID:
-        await message.answer("Вы уже Пользователь")
+        await message.answer("Вы уже Пользователь!")
     else:
         await message.answer("Переключение на профиль пользователя...")
         await af.clear_admin_id(AdminsList.ADMIN_ID)
@@ -68,7 +68,7 @@ async def switch_to_user(message: types.Message, state: FSMContext):
 @router.message(AuthStates.USER, Command("admin"))
 async def switch_to_admin(message: types.Message, state: FSMContext):
     if message.from_user.id in AdminsList.ADMIN_ID:
-        await message.answer("Вы уже Администратор")
+        await message.answer("Вы уже Администратор!")
     else:
         await message.answer("Вы вошли в режим авторизации. \n\rВведите пароль")
         await state.set_state(AuthStates.wait_pass)
@@ -78,7 +78,7 @@ async def switch_to_admin(message: types.Message, state: FSMContext):
 @router.message(AuthStates.wait_pass, Command("cancel"))
 async def cancel_authorization(message: types.Message, state: FSMContext):
     await state.set_state(AuthStates.USER)
-    await message.answer("Вы вышли из режима авторизации")
+    await message.answer("Вы вышли из режима авторизации.")
 
 
 # Попытка авторизации
@@ -99,10 +99,10 @@ async def admin_command(message: types.Message, state: FSMContext):
     if message.from_user.id in AdminsList.ADMIN_ID:
         await state.set_state(AuthStates.wait_file)
         await message.answer("Вы вошли в режим загрузки.\
-                             \n\rЗагрузите файл в формате xls")
+                             \n\rЗагрузите файл в формате xls.")
 
     else:
-        await message.answer("У вас нет доступа к данной команде")
+        await message.answer("У вас нет доступа к данной команде.")
 
 
 # Загрузка на Яндекс.Диск файла, добавленного администратором
@@ -112,23 +112,23 @@ async def handle_document(message: types.Message, state: FSMContext):
     name_length = len(name_of_file)
     if name_of_file.endswith(file_extension, 0, name_length):
         file_id = message.document.file_id
-        success = await crd.async_upload_to_yandex(file_id, message.document.file_name)
+        success = await crd.async_upload_to_yandex(configuration['DIRECTORY'], file_id, message.document.file_name)
         if success:
             await state.set_state(AuthStates.ADMIN)
             await message.reply("Файл успешно загружен на Яндекс.Диск!")
 
         else:
-            await message.reply("Произошла ошибка при загрузке файла на Яндекс.Диск")
+            await message.reply("Произошла ошибка при загрузке файла на Яндекс.Диск.")
     else:
         await message.answer("Некорректный формат документа.\
-                             \n\rК загрузке допустимы файлы с расширением .xls/.xlsx")
+                             \n\rК загрузке допустимы файлы с расширением .xls/.xlsx.")
 
 
 # Выход из режима загрузки файлов
 @router.message(AuthStates.wait_file, Command('cancel_upload'))
 async def cancel_upload_file(message: types.Message, state: FSMContext):
     await state.set_state(AuthStates.ADMIN)
-    await message.answer("Вы вышли из режима загрузки")
+    await message.answer("Вы вышли из режима загрузки.")
 
 
 # Обработка случайных сигналов в режиме ожидания файла
@@ -142,18 +142,23 @@ async def wait_file_interceptor(message: types.Message):
 @router.message(AuthStates.ADMIN, Command("delete_file"))
 async def delete_file_cmd(message: types.Message):
     if message.from_user.id in AdminsList.ADMIN_ID:
-        await message.answer("Файл стёрт с удалённого диска")
+        response = await crd.delete_from_yandex_disk(configuration['DIRECTORY'])
+        if response:
+            await message.answer(response)
+        else:
+            await message.answer("Произошла ошибка во время удаления файла с Яндекс.Диска.")
+
     else:
-        await message.answer("У вас нет доступа к данной команде")
+        await message.answer("У вас нет доступа к данной команде.")
 
 
 # Команды, доступные только администратору
 @router.message(AuthStates.ADMIN, Command("modify_file"))
 async def modify_file_cmd(message: types.Message):
     if message.from_user.id in AdminsList.ADMIN_ID:
-        await message.answer("Файл изменён")
+        await message.answer("Файл изменён!")
     else:
-        await message.answer("У вас нет доступа к данной команде")
+        await message.answer("У вас нет доступа к данной команде.")
 
 
 # Функция, которая доступна как администратору, так и пользователю
@@ -162,7 +167,17 @@ async def read_data(message: types.Message):
     await message.answer('Выполняю выгрузку данных...')
 
 
+# Функция, доступная как администратору, так и пользователю
+@router.message(StateFilter(AuthStates.ADMIN, AuthStates.USER), Command("check_disk"))
+async def check_disk_data(message: types.Message):
+    response = await crd.check_yandex_disk(configuration['DIRECTORY'])
+    if response:
+        await message.answer(response)
+    else:
+        await message.answer("Произошла ошибка при проверке файлов на Яндекс.Диске.")
+
+
 # Обработка случайных сигналов
 @router.message(StateFilter(AuthStates.ADMIN, AuthStates.USER))
 async def signal_interceptor(message: types.Message):
-    await message.answer('Команда не распознана. Используйте доступные вам функции')
+    await message.answer("Команда не распознана. Используйте доступные вам функции.")
--- rostelecom_bot/logic/crud.py
+++ rostelecom_bot/logic/crud.py
@@ -10,81 +10,91 @@ from yadisk import YaDisk
 from rostelecom_bot.utils.config import configuration
 from rostelecom_bot.main import TgBot
 
-# Инициализация асинхронного клиента Яндекс.Диска
-client = AsyncClient(token=configuration['YANDEX_TOKEN'])
 
-
-async def async_upload_to_yandex(file_id: str, file_name):
+async def async_upload_to_yandex(folder_path: str, file_id: str, file_name) -> bool:
+    """Данная функция создаёт директорию для сохранения файла, если таковая отсутствует,
+    и загружает в неё файл, отправленный администратором"""
+    client = AsyncClient(token=configuration['YANDEX_TOKEN'])
     async with client:
         if await client.check_token():
-
             try:
                 file_info = await TgBot.bot.get_file(file_id)
                 file_path = file_info.file_path
 
-                if not await client.is_dir('/RostelecomBot'):
-                    await client.mkdir('/RostelecomBot')
+                if not await client.is_dir(folder_path):
+                    await client.mkdir(folder_path)
                 
-                src = '/RostelecomBot/' + file_name
+                src = folder_path + '/' + file_name
 
                 if await client.is_file(src):
-                    src = '/RostelecomBot/Double-' + datetime.now().strftime("%d.%m.%Y-%H.%M.%S") + '-' + file_name
+                    src = folder_path + '/Double-' + datetime.now().strftime("%d.%m.%Y-%H.%M.%S") + '-' + file_name
                 
                 await client.upload(await TgBot.bot.download_file(file_path), src)
                 
                 return True
-            
             except Exception as e:
                 print('Ошибка', e)
-        
+                return False
         else:
             return False
-        
-        
-"""
-# Инициализация клиента Яндекс.Диска
-y = YaDisk(token=configuration['YANDEX_TOKEN'])
 
 
-async def upload_to_yandex_disk(file_id: str):
-    #Данная функция реализует загрузку полученного файла на Яндекс.Диск
-    if y.check_token():
-
-        try:
-            # Получение информации о файле из Telegram
-            file_info = await TgBot.bot.get_file(file_id)
-            file_path = file_info.file_path
-
-            # Получение URL для загрузки файла
-            file_url = f"https://api.telegram.orgile/bot{configuration['BOT_TOKEN']}/{file_path}"
-            
-            # Инициализация клиента aiohttp
-            async with aiohttp.ClientSession() as session:
-                async with session.get(file_url) as response:
-                    if response.status == 200:
-
-                        # Загрузка файла на Яндекс.Диск
-                        async with  y.upload(f"/client/disk/mipt_dataset/{file_path}") as f:
-                            await f.write(await response.read())
-                            return True
+async def check_yandex_disk(folder_path: str) -> str | bool:
+    """Данная функция проверяет наличие файлов и их количество
+    при присутствии в указанной директории """
+    client = AsyncClient(token=configuration['YANDEX_TOKEN'])
+    async with client:
+        if await client.check_token():
+            try:
+                if await client.is_dir(folder_path):
+                    files = await client.listdir(folder_path)
+                    file_list = [i async for i in files]
+                    if len(file_list) == 0:
+                        return f"Файлов не обнаружено."
                     else:
-                        return False
-                    
-        except Exception as e:
-            print("Ошибка при загрузке файла на Яндекс.Диск", e)
-    
-    else:
-        print("Яндекс.Диск недоступен. Проверьте корректность токена и работоспособность диска")
-"""
+                        result = f"Обнаружено файлов: {len(file_list)}\n\r\n\r"
+                        for file in file_list:
+                            result += f"{file['name']}\n\r"
+                        return result
+                else:
+                    return f"Указанная папка не существует.\n\rЧтение невозможно!"
+            except Exception as e:
+                print('Ошибка', e)
+                return False
+        else:
+            return False
 
-async def delete_from_yandex_disk():
-    pass
+
+async def delete_from_yandex_disk(folder_path) -> str | bool:
+    """Данная функция реализует удаление файлов из указанной директории"""
+    client = AsyncClient(token=configuration['YANDEX_TOKEN'])
+    async with client:
+        if await client.check_token():
+            try:
+                if await client.is_dir(folder_path):              
+                    files = await client.listdir(folder_path)
+                    file_list = [i async for i in files]
+                    if len(file_list) > 0:
+                        for file in file_list:
+                            await client.remove(file['path'], permanently=True)
+                            return f"Все файлы успешно удалены!"
+                    else:
+                        return f"Файлов для удаления не найдено."
+                else:
+                    return f"Указанная папка не существует.\n\rУдаление невозможно!"
+            except Exception as e:
+                print("Ошибка", e)
+                return False
+        return False
+
 
 async def read_from_yandex_disk():
     pass
 
+
 async def upload_from_yandex_disk():
     pass
+
 
 async def modify_through_bot():
     pass
